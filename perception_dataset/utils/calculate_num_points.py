afa45c8fb1ffa3f80bfcb2778d83be79ee53a87d
Shunsuke Miura <shunsuke.miura@tier4.jp>
2023-08-23 13:25:58 +0900

    fix
    Signed-off-by: Shunsuke Miura <shunsuke.miura@tier4.jp>

--- perception_dataset/utils/calculate_num_points.py
+++ perception_dataset/utils/calculate_num_points.py
@@ -17,7 +17,7 @@ def calculate_num_points(
     """Calcluate number of points in each box and overwrite the annotation table"""
     nusc = NuScenes(version="annotation", dataroot=dataroot, verbose=False)
     for sample in nusc.sample:
-        if not lidar_sensor_channel in sample["data"]:
+        if lidar_sensor_channel not in sample["data"]:
             continue
         lidar_token = sample["data"][lidar_sensor_channel]
         lidar_path, boxes, _ = nusc.get_sample_data(lidar_token)
