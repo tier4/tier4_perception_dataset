5d22af22d9e75da2ae053518c9401b0cd05556d1
Magdalena Kotynia <magdalena.kotynia@robotec.ai>
2023-09-12 11:01:19 +0200

    check of CI

--- perception_dataset/utils/rosbag2.py
+++ perception_dataset/utils/rosbag2.py
@@ -6,6 +6,7 @@ from typing import Any, Dict, List, Optional, Tuple
 import uuid
 
 import builtin_interfaces.msg
+import cv2
 from nptyping import NDArray
 import numpy as np
 from radar_msgs.msg import RadarTrack, RadarTracks
@@ -161,13 +162,21 @@ def radar_tracks_msg_to_list(radar_tracks_msg: RadarTracks) -> List[Dict[str, An
     return radar_tracks
 
 
-def compressed_msg_to_numpy(compressed_image_msg: CompressedImage) -> np.ndarray:
-    try:
-        np_arr = np.frombuffer(compressed_image_msg.data, np.uint8)
-        image = np.reshape(np_arr, (compressed_image_msg.height, compressed_image_msg.width, 3))
-    except Exception as e:
-        print(e)
-        return None
+# def compressed_msg_to_numpy(compressed_image_msg: CompressedImage) -> np.ndarray:
+#     try:
+#         np_arr = np.frombuffer(compressed_image_msg.data, np.uint8)
+#         image = np.reshape(np_arr, (compressed_image_msg.height, compressed_image_msg.width, 3))
+#     except Exception as e:
+#         print(e)
+#         return None
+#     return image
+
+
+def compressed_msg_to_numpy(compressed_image_msg: CompressedImage) -> NDArray:
+    image_buf = np.ndarray(
+        shape=(1, len(compressed_image_msg.data)), dtype=np.uint8, buffer=compressed_image_msg.data
+    )
+    image = cv2.imdecode(image_buf, cv2.IMREAD_ANYCOLOR)
     return image
 
 
