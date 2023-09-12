e42ea8cadb03735fc267195131edf9a42ff70d1c
Magdalena Kotynia <magdalena.kotynia@robotec.ai>
2023-09-12 13:00:57 +0200

    check of CI

--- perception_dataset/rosbag2/rosbag2_to_non_annotated_t4_converter.py
+++ perception_dataset/rosbag2/rosbag2_to_non_annotated_t4_converter.py
@@ -780,7 +780,8 @@ class _Rosbag2ToNonAnnotatedT4Converter:
                     "y": rotation.y,
                     "z": rotation.z,
                 }
-                cam_info_topic = os.path.dirname(topic_name) + "/camera_info"
+                # cam_info_topic = os.path.dirname(topic_name) + "/camera_info"
+                cam_info_topic = topic_name.rsplit("/", 2)[0] + "/camera_info"
                 info = self._bag_reader.camera_info.get(cam_info_topic)
                 if info is None:
                     continue
