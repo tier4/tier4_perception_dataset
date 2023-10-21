2e4b411da6dc33ef3e8f4b662c8538ffefcc9fab
Shunsuke Miura <shunsuke.miura@tier4.jp>
2023-10-21 17:03:31 +0900

    refactor
    Signed-off-by: Shunsuke Miura <shunsuke.miura@tier4.jp>

--- perception_dataset/t4_dataset/keyframe_fixer.py
+++ perception_dataset/t4_dataset/keyframe_fixer.py
@@ -13,7 +13,32 @@ class KeyFrameFixer:
             sample_data_list = json.load(f)
         with open(segment_path / "annotation/sample.json", "r") as f:
             sample_list = json.load(f)
+        with open(segment_path / "annotation/scene.json", "r") as f:
+            scene_list = json.load(f)
 
+        # remove sample that sample_data is not a keyframe
+        self._remove_sample_of_non_keyframe(sample_data_list, sample_list, sample_annotation_list)
+
+        # change non-keyframe sample_token in sample_data to next closest keyframe
+        self._change_sample_token_to_next_closest_keyframe(sample_data_list)
+
+        self.connect_sample_data_next_prev_tokens(sample_data_list)
+
+        self.connect_sample_next_prev_tokens(sample_list)
+
+        scene_list[0]["first_sample_token"] = sample_list[0]["token"]
+        scene_list[0]["last_sample_token"] = sample_list[-1]["token"]
+
+        with open(segment_path / "annotation/scene.json", "w") as f:
+            json.dump(scene_list, f, indent=4)
+        with open(segment_path / "annotation/sample.json", "w") as f:
+            json.dump(sample_list, f, indent=4)
+        with open(segment_path / "annotation/sample_data.json", "w") as f:
+            json.dump(sample_data_list, f, indent=4)
+
+    def _remove_sample_of_non_keyframe(
+        self, sample_data_list: list, sample_list: list, sample_annotation_list: list
+    ):
         for sample_data in sample_data_list:
             corresponding_annotation = [
                 sample_anno
@@ -26,10 +51,11 @@ class KeyFrameFixer:
 
             # If there is no corresponding annotation, then it is not a keyframe
             if len(corresponding_annotation) == 0:
+                print(f"Sample data {sample_data['token']} has no corresponding annotation")
                 # change is_keyframe to False
                 sample_data["is_key_frame"] = False
 
-                # change sample_token to ""
+                # change sample_token to "" (temporarily)
                 sample_data["sample_token"] = ""
 
                 # remove from sample
@@ -38,6 +64,7 @@ class KeyFrameFixer:
                 corresponding_sample = corresponding_sample_list[0]
                 sample_list.remove(corresponding_sample)
 
+    def _change_sample_token_to_next_closest_keyframe(self, sample_data_list: list):
         for sample_data in sample_data_list[:]:
             if sample_data["is_key_frame"]:
                 continue
@@ -47,10 +74,35 @@ class KeyFrameFixer:
             if sample_data["sample_token"] == "":
                 sample_data_list.remove(sample_data)
 
-        with open(segment_path / "annotation/sample.json", "w") as f:
-            json.dump(sample_list, f, indent=4)
-        with open(segment_path / "annotation/sample_data.json", "w") as f:
-            json.dump(sample_data_list, f, indent=4)
+    def connect_sample_next_prev_tokens(self, sample_list: list):
+        sample_list = sorted(sample_list, key=lambda x: x["timestamp"])
+        sample_list[0]["prev"] = ""
+        sample_list[-1]["next"] = ""
+
+        for sample_i in range(1, len(sample_list)):
+            cur_sample = sample_list[sample_i]
+            prev_sample = sample_list[sample_i - 1]
+
+            prev_sample["next"] = cur_sample["token"]
+            cur_sample["prev"] = prev_sample["token"]
+
+    def connect_sample_data_next_prev_tokens(self, sample_data_list: list):
+        sensor_list = []
+        [
+            sensor_list.append(data["calibrated_sensor_token"])
+            for data in sample_data_list
+            if not data["calibrated_sensor_token"] in sensor_list
+        ]
+
+        new_sample_data_list = []
+        for sensor in sensor_list:
+            sensor_sample_data_list = [
+                data for data in sample_data_list if data["calibrated_sensor_token"] == sensor
+            ]
+            self.connect_sample_next_prev_tokens(sensor_sample_data_list)
+            new_sample_data_list.extend(sensor_sample_data_list)
+
+        sample_data_list = new_sample_data_list
 
     def _get_next_closest_keyframe(self, current_sample_data: dict, sample_data_list: list):
         timestamp = current_sample_data["timestamp"]
