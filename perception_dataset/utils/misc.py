4df8c373100faee371bbdd255f5a5aeb47c5f98a
kminoda <koji.minoda@tier4.jp>
2023-12-08 15:56:10 +0900

    feat: finalize refactoring, now testing
    Signed-off-by: kminoda <koji.minoda@tier4.jp>

--- perception_dataset/utils/misc.py
+++ perception_dataset/utils/misc.py
@@ -1,4 +1,8 @@
 import os
+import numpy as np
+from typing import List
+from sensor_msgs.msg import PointCloud2
+import perception_dataset.utils.rosbag2 as rosbag2_utils
 
 from perception_dataset.constants import T4_FORMAT_DIRECTORY_NAME
 
@@ -18,3 +22,67 @@ def get_sample_data_filename(sensor_channel: str, frame_index: int, fileformat: 
         f"{frame_index:05}.{fileformat}",
     )
     return filename
+
+def get_points_from_pointcloud_msg(pointcloud_msg):
+    if isinstance(pointcloud_msg, PointCloud2):
+        points_arr = rosbag2_utils.pointcloud_msg_to_numpy(pointcloud_msg)
+    else:
+        points_arr = np.zeros((0, 5), dtype=np.float32)
+    return points_arr
+
+def get_lidar_camera_synced_frame_info(
+    lidar_timestamp_list: List[float],
+    image_timestamp_list: List[float],
+    start_timestamp: float = 0.0,
+    camera_latency_sec: float = 0.0,
+    accept_frame_drop: bool = False,
+    timestamp_diff: float = 0.15,
+    num_load_frames: int = 0,
+):
+    synced_frame_info_list: List[int, int, bool] = []  # [image_index, lidar_frame_index, dummy_timestamp (None if not dummy)]
+    frame_index: int = 0
+    generated_frame_index: int = 0
+    prev_frame_unix_timestamp = start_timestamp
+    for image_index, image_unix_timestamp in enumerate(image_timestamp_list):
+        if generated_frame_index >= num_load_frames:
+            break
+
+        # Get LiDAR data
+        lidar_unix_timestamp = lidar_timestamp_list[frame_index]
+
+        # Address image drop
+        while (image_unix_timestamp - prev_frame_unix_timestamp) > timestamp_diff:
+            # Add dummy image timestamp in synced_frame_info_list
+            dummy_image_timestamp = image_unix_timestamp
+            while (dummy_image_timestamp - prev_frame_unix_timestamp) > timestamp_diff:
+                dummy_image_timestamp -= 0.1
+            synced_frame_info_list.append([image_index, generated_frame_index, dummy_image_timestamp])
+
+            # Increment LiDAR information
+            frame_index += 1
+            generated_frame_index += 1
+            prev_frame_unix_timestamp = dummy_image_timestamp
+            if generated_frame_index >= num_load_frames:
+                return synced_frame_info_list
+
+            lidar_unix_timestamp = lidar_timestamp_list[generated_frame_index]
+
+        time_diff_from_lidar = image_unix_timestamp - lidar_unix_timestamp
+        if not accept_frame_drop and time_diff_from_lidar > (
+            camera_latency_sec + timestamp_diff
+        ):
+            raise ValueError(
+                f"Topic message may be dropped at [{generated_frame_index}]: lidar_timestamp={lidar_unix_timestamp} image_timestamp={image_unix_timestamp}"
+            )
+
+        print(
+            f"frame{generated_frame_index}, stamp = {image_unix_timestamp}, diff cam - lidar = {time_diff_from_lidar:0.3f} sec"
+        )
+
+        print(f"frame{generated_frame_index}, image stamp: {image_unix_timestamp}")
+        synced_frame_info_list.append([image_index, generated_frame_index, None])
+        generated_frame_index += 1
+        prev_frame_unix_timestamp = image_unix_timestamp
+        frame_index += 1
+
+    return synced_frame_info_list
--- tests/utils/test_misc.py
+++ tests/utils/test_misc.py
@@ -1,3 +1,5 @@
+import perception_dataset.utils.misc as misc_utils
+
 def test_unix_timestamp_to_nusc_timestamp():
     # TODO(yukke42): impl test_unix_timestamp_to_nusc_timestamp
     pass
@@ -11,3 +13,19 @@ def test_nusc_timestamp_to_unix_timestamp():
 def test_get_sample_data_filename():
     # TODO(yukke42): impl test_unix_timestamp_to_nusc_timestamp
     pass
+
+def test_get_lidar_camera_synced_frame_info_1():
+    lidar_timestamp_list = [0.0, 0.1, 0.2, 0.3, 0.4]
+    image_timestamp_list = [0.0, 0.2, 0.4]
+    synced_frame_info_list = misc_utils.get_lidar_camera_synced_frame_info(
+        lidar_timestamp_list=lidar_timestamp_list,
+        image_timestamp_list=image_timestamp_list,
+        start_timestamp=0.0,
+        camera_latency_sec=0.0,
+        accept_frame_drop=False,
+        timestamp_diff=0.15,
+        num_load_frames=0,
+    )
+
+    expected = [[0, 0, None], [2, 1, None], [4, 2, None]]
+    assert synced_frame_info_list == expected
