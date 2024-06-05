fb1fd5725f37918b70b32e099c34df0dc4f5f3c8
kminoda <44218668+kminoda@users.noreply.github.com>
2024-06-05 19:54:52 +0900

    Revert "feat: add backward compatibility for autoware_auto_msgs (#104)" (#109)
    This reverts commit 4ce94fc86c00b008d02265e4387837f14fc1dcf4.

--- perception_dataset/rosbag2/autoware_msgs.py
+++ perception_dataset/rosbag2/autoware_msgs.py
@@ -1,27 +1,13 @@
 from typing import Any, Dict, List, Optional, Union
 import uuid
 
-try:
-    from autoware_auto_perception_msgs.msg import (
-        DetectedObject,
-        DetectedObjects,
-        ObjectClassification,
-        TrackedObject,
-        TrackedObjects,
-    )
-
-    print("Use autoware_auto_perception_msgs")
-except ModuleNotFoundError:
-    from autoware_perception_msgs.msg import (
-        DetectedObject,
-        DetectedObjects,
-        ObjectClassification,
-        TrackedObject,
-        TrackedObjects,
-    )
-
-    print("autoware_auto_perception_msgs not found. Use autoware_perception_msgs")
-
+from autoware_auto_perception_msgs.msg import (
+    DetectedObject,
+    DetectedObjects,
+    ObjectClassification,
+    TrackedObject,
+    TrackedObjects,
+)
 from tier4_perception_msgs.msg import (
     TrafficLight,
     TrafficLightArray,
