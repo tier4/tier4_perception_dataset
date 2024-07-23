fa0069677f3b33aa52269a39b5e3516eaac01d47
Shunsuke Miura <37187849+miursh@users.noreply.github.com>
2024-07-23 17:24:04 +0900

    Update perception_dataset/t4_dataset/format_validator.py
    Co-authored-by: kminoda <44218668+kminoda@users.noreply.github.com>

--- perception_dataset/t4_dataset/format_validator.py
+++ perception_dataset/t4_dataset/format_validator.py
@@ -81,7 +81,7 @@ def validate_sample(nusc: NuScenes):
 
     no_next_token_count: int = 0
     no_prev_token_count: int = 0
-    sample_annotation_refered_sample_tokens = [
+    sample_annotation_referred_sample_tokens = [
         ann["sample_token"] for ann in nusc.sample_annotation
     ]
     for sample in nusc.sample:
