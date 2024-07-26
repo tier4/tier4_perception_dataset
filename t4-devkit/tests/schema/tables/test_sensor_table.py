2698f8fdf38dd10e4402c049e5330b40fe5d55fd
Kotaro Uetake <60615504+ktro2828@users.noreply.github.com>
2024-07-26 11:13:53 +0900

    feat(t4-devkit): remove `SensorChannel` and replace it by `str` (#132)
    * feat: remove `SensorChannel` and replace it with `str`
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * docs: update documents
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * fix: invalid method access
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    ---------
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- t4-devkit/tests/schema/tables/test_sensor_table.py
+++ t4-devkit/tests/schema/tables/test_sensor_table.py
@@ -1,4 +1,4 @@
-from t4_devkit.schema import SensorChannel, SensorModality
+from t4_devkit.schema import SensorModality
 
 
 def test_sensor_modality() -> None:
@@ -13,38 +13,3 @@ def test_sensor_modality() -> None:
     # check each member can construct
     for value in modalities:
         _ = SensorModality(value)
-
-
-def test_sensor_channel() -> None:
-    """Test SensorChannel enum."""
-
-    # {channel: modality}
-    sensor_channels = {
-        "CAM_FRONT": SensorModality.CAMERA,
-        "CAM_FRONT_RIGHT": SensorModality.CAMERA,
-        "CAM_FRONT_LEFT": SensorModality.CAMERA,
-        "CAM_BACK": SensorModality.CAMERA,
-        "CAM_BACK_RIGHT": SensorModality.CAMERA,
-        "CAM_BACK_LEFT": SensorModality.CAMERA,
-        "CAM_TRAFFIC_LIGHT_NEAR": SensorModality.CAMERA,
-        "CAM_TRAFFIC_LIGHT_FAR": SensorModality.CAMERA,
-        "LIDAR_TOP": SensorModality.LIDAR,
-        "LIDAR_CONCAT": SensorModality.LIDAR,
-        "RADAR_FRONT": SensorModality.RADAR,
-        "RADAR_FRONT_RIGHT": SensorModality.RADAR,
-        "RADAR_FRONT_LEFT": SensorModality.RADAR,
-        "RADAR_BACK": SensorModality.RADAR,
-        "RADAR_BACK_RIGHT": SensorModality.RADAR,
-        "RADAR_BACK_LEFT": SensorModality.RADAR,
-    }
-
-    # check all enum members are covered by above names
-    members: list[str] = [m.value for m in SensorChannel]
-    assert set(members) == set(sensor_channels.keys())
-
-    # check each member can construct and its method is valid
-    for channel, modality in sensor_channels.items():
-        member = SensorChannel(channel)
-
-        # check modality
-        assert member.modality == modality
