2a0aef9f5a002c3b5ec12f75ff4145cecacc6d97
Kotaro Uetake <60615504+ktro2828@users.noreply.github.com>
2024-11-22 06:44:53 +0900

    feat: update record classes for CoMLOPs format (#163)
    * feat: update record classes for CoMLOPs format
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * ci: update test depends
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * Update perception_dataset/ros2/oxts_msgs/ins_handler.py
    
    Co-authored-by: Shunsuke Miura <37187849+miursh@users.noreply.github.com>
    
    * feat: update acceleration coordinate system in base_link
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * feat: update handling of velocity/omega/acceleration
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * chore: remove unused tf-transformations
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * ci: update to install pacmod3-msgs
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * ci: update to install libboost-dev
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * feat: set optional values to None by default
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    ---------
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    Co-authored-by: Shunsuke Miura <37187849+miursh@users.noreply.github.com>

--- perception_dataset/t4_dataset/classes/ego_pose.py
+++ perception_dataset/t4_dataset/classes/ego_pose.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from perception_dataset.constants import EXTENSION_ENUM
 from perception_dataset.t4_dataset.classes.abstract_class import AbstractRecord, AbstractTable
@@ -10,15 +10,28 @@ class EgoPoseRecord(AbstractRecord):
         translation: Dict[str, float],
         rotation: Dict[str, float],
         timestamp: int,
+        twist: Optional[Dict[str, float]] = None,
+        acceleration: Optional[Dict[str, float]] = None,
+        geocoordinate: Optional[Dict[str, float]] = None,
     ):
         super().__init__()
 
         assert {"x", "y", "z"} == set(translation.keys())
         assert {"w", "x", "y", "z"} == set(rotation.keys())
 
+        if twist is not None:
+            assert {"vx", "vy", "vz", "yaw_rate", "pitch_rate", "roll_rate"} == set(twist.keys())
+        if acceleration is not None:
+            assert {"ax", "ay", "az"} == set(acceleration.keys())
+        if geocoordinate is not None:
+            assert {"latitude", "longitude", "altitude"} == set(geocoordinate.keys())
+
         self.translation: Dict[str, float] = translation
         self.rotation: Dict[str, float] = rotation
         self.timestamp: int = timestamp
+        self.twist: Optional[Dict[str, float]] = twist
+        self.acceleration: Optional[Dict[str, float]] = acceleration
+        self.geocoordinate = geocoordinate
 
     def to_dict(self) -> Dict[str, Any]:
         d = {
@@ -35,6 +48,36 @@ class EgoPoseRecord(AbstractRecord):
                 self.rotation["z"],
             ],
             "timestamp": self.timestamp,
+            "twist": (
+                [
+                    self.twist["vx"],
+                    self.twist["vy"],
+                    self.twist["vz"],
+                    self.twist["yaw_rate"],
+                    self.twist["pitch_rate"],
+                    self.twist["roll_rate"],
+                ]
+                if self.twist is not None
+                else None
+            ),
+            "acceleration": (
+                [
+                    self.acceleration["ax"],
+                    self.acceleration["ay"],
+                    self.acceleration["az"],
+                ]
+                if self.acceleration is not None
+                else None
+            ),
+            "geocoordinate": (
+                [
+                    self.geocoordinate["latitude"],
+                    self.geocoordinate["longitude"],
+                    self.geocoordinate["altitude"],
+                ]
+                if self.geocoordinate is not None
+                else None
+            ),
         }
         return d
 
