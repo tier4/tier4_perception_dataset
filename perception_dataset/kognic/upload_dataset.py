086afa4ff3794545e30a49420f3acd9318536522
natanaels12 <natanaelsuwandi@outlook.com>
2026-06-30 12:05:52 +0900

    fix: formatter error

--- perception_dataset/kognic/upload_dataset.py
+++ perception_dataset/kognic/upload_dataset.py
@@ -741,7 +741,7 @@ class KognicDatasetUploader:
         ego_poses: Optional[Dict[str, KognicModel.EgoVehiclePose]],
     ) -> List[IMUData]:
         if not self.config.include_imu_data or not ego_poses:
-            logger.info(f"Skipping building the IMU data...")
+            logger.info("Skipping building the IMU data...")
             return []
 
         try:
@@ -751,7 +751,7 @@ class KognicDatasetUploader:
             logger.warning("scipy is not installed; skipping optional IMU data generation")
             return []
 
-        logger.info(f"Building IMU data for (this may take a while if there are many frames)")
+        logger.info("Building IMU data for (this may take a while if there are many frames)")
 
         sparse: List[Tuple[int, KognicModel.EgoVehiclePose]] = []
         for frame_id, timestamp_ns, _ in self.iterate_frames(sequence_path):
